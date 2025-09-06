7f5e81979f21fe1ba6f5aeaf26ba01cb8930bf80
avvienash <avvienash@gmail.com>
2025-09-06 23:31:47 +1000

    Made App PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+            tailwindcss(),
+            VitePWA({
+            registerType: "autoUpdate",   
+            manifest: {
+              name: "NewsFlash Quiz",
+              short_name: "NQ",
+              description: "newsflash quiz app",
+              theme_color: "#0d1117",
+              background_color: "#ffffff",
+              display: "standalone",
+              start_url: "/",
+              icons: [
+                {
+                  src: "/web-app-manifest-192x192.png",
+                  sizes: "192x192",
+                  type: "image/png",
+                },
+                {
+                  src: "/web-app-manifest-512x512.png",
+                  sizes: "512x512",
+                  type: "image/png",
+                },
+              ],
+            },
+          })],
   base: '/', //'/QuizApp-frontend/',
 })
+
+
